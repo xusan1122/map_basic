b999789366b4f55eb73bb1f84f9f80e430eecaea
Olimjon Yakhshiboyev <63545453+OlimjonSN@users.noreply.github.com>
2022-07-23 12:00:37 +0500

    Update map_basic03.dart

--- bin/map_basic03.dart
+++ bin/map_basic03.dart
@@ -1,6 +1,6 @@
 /*
   
-  Given argument data type of Map check it length greater than zero 
+  Given an argument type of Map.
 Return true if the length of the map is greater than zero, otherwise false.
 
 
